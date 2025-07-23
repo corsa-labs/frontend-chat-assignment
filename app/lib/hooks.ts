14d7637a49fbdbd05be543b97e1ec535e0e7435e
tom-voicer <tom@voicer.com>
2025-07-23 14:25:08 +0300

    ..

---
+++ app/lib/hooks.ts
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
+export const useChats = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleGetChats = async (
+    page: number,
+    pageSize: number,
+    search: string
+  ) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        `/api/chats?page=${page}&pageSize=${pageSize}&search=${encodeURIComponent(
+          search
+        )}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch chats");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      setError(error instanceof Error ? error.message : "An error occurred");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    loading,
+    error,
+    handleGetChats,
+  };
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+
+import { useCallback, useEffect, useRef, useState } from "react";
+import { IChatPreview } from "./types";
+import { useChats } from "./lib/hooks";
+
+const PAGE_SIZE = 15;
+
 export default function Home() {
+  const initialFetch = useRef(false);
+  const [chats, setChats] = useState<IChatPreview[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [selectedChatId, setSelectedChatId] = useState<string | null>(null);
+
+  const { loading, error, handleGetChats } = useChats();
+
+  const fetchChats = useCallback(
+    async (page = currentPage) => {
+      const response = await handleGetChats(page, PAGE_SIZE, search);
+      setChats((prev) => [...prev, ...response.chats]);
+    },
+    [currentPage, handleGetChats, search]
+  );
+
+  useEffect(() => {
+    if (!initialFetch.current) {
+      fetchChats();
+      initialFetch.current = true;
+    }
+  }, [fetchChats]);
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-4xl font-bold">Frontend chat assignment</h1>
+      <button
+        onClick={() => {
+          const newPage = currentPage + 1;
+          setCurrentPage(newPage);
+          fetchChats(newPage);
+        }}
+      >
+        next
+      </button>
+      <div className="flex flex-col">
+        {chats.map((chat) => (
+          <div key={chat.id}>{chat.contactName}</div>
+        ))}
+      </div>
     </div>
   );
 }
